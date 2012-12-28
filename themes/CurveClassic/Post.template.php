d47a500c1844811983778662bf4db140a41fb359
emanuele <emanuele45@gmail.com>
2012-12-28 21:47:46 +0100

    Updated CurveClassic
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- themes/CurveClassic/Post.template.php
+++ themes/CurveClassic/Post.template.php
@@ -104,50 +104,34 @@ function template_main()
 	echo '
 					<dl id="post_header">';
 
-	// Guests have to put in their name and email...
-	if (isset($context['name']) && isset($context['email']))
-	{
-		echo '
-						<dt>
-							<span', isset($context['post_error']['long_name']) || isset($context['post_error']['no_name']) || isset($context['post_error']['bad_name']) ? ' class="error"' : '', ' id="caption_guestname">', $txt['name'], ':</span>
-						</dt>
-						<dd>
-							<input type="text" name="guestname" size="25" value="', $context['name'], '" tabindex="', $context['tabindex']++, '" class="input_text" />
-						</dd>';
-
-		if (empty($modSettings['guest_post_no_email']))
+	if (!empty($context['post_above']))
+		foreach ($context['post_above'] as $id => $value)
+		{
 			echo '
-						<dt>
-							<span', isset($context['post_error']['no_email']) || isset($context['post_error']['bad_email']) ? ' class="error"' : '', ' id="caption_email">', $txt['email'], ':</span>
-						</dt>
-						<dd>
-							<input type="text" name="email" size="25" value="', $context['email'], '" tabindex="', $context['tabindex']++, '" class="input_text" />
-						</dd>';
-	}
+							<dt>
+								<span', isset($context['post_error']['long_name']) || isset($context['post_error']['no_' . $value['id']]) || isset($context['post_error']['bad_' . $value['id']]) ? ' class="error"' : '', ' id="', $id, '">', $value['text'], ':</span>
+							</dt>';
+			if ($value['type'] == 'text')
+				echo '
+							<dd>
+								<input type="text" name="', $value['post_name'], '" size="25" value="', $value['value'], '" tabindex="', $context['tabindex']++, '" class="input_text" />
+							</dd>';
+			elseif ($value['type'] == 'select')
+			{
+				echo '
+							<dd id="container_', $id, '">
+								<select id="', $id, '" name="', $value['post_name'], '" tabindex="', $context['tabindex']++, '"', !empty($value['options']) ? $value['options'] : '', '>';
+				foreach ($value['value'] as $key => $val)
+					echo '
+									<option value="', $val['value'], '"', $val['value'] == $context['icon'] ? ' selected="selected"' : '', '>', $val['name'], '</option>';
 
-	// Now show the subject box for this post.
-	echo '
-						<dt class="clear">
-							<span', isset($context['post_error']['no_subject']) ? ' class="error"' : '', ' id="caption_subject">', $txt['subject'], ':</span>
-						</dt>
-						<dd>
-							<input id="post_subject" type="text" name="subject"', $context['subject'] == '' ? '' : ' value="' . $context['subject'] . '"', ' tabindex="', $context['tabindex']++, '" size="80" maxlength="80"', isset($context['post_error']['no_subject']) ? ' class="error"' : ' class="input_text"', '/>
-						</dd>
-						<dt class="clear_left">
-							', $txt['message_icon'], ':
-						</dt>
-						<dd>
-							<select name="icon" id="icon" onchange="showimage()">';
-
-	// Loop through each message icon allowed, adding it to the drop down list.
-	foreach ($context['icons'] as $icon)
-		echo '
-								<option value="', $icon['value'], '"', $icon['value'] == $context['icon'] ? ' selected="selected"' : '', '>', $icon['name'], '</option>';
+				echo '
+								</select>', !empty($value['after_control']) ? $value['after_control'] : '', '
+							</dd>';
+			}
+		}
 
 	echo '
-							</select>
-							<img src="', $context['icon_url'], '" name="icons" hspace="15" alt="" />
-						</dd>
 					</dl>
 					<hr class="clear" />';
 
@@ -742,4 +726,88 @@ function template_quotefast()
 		// ]]></script>
 	</body>
 </html>';
+}
+
+function template_announce()
+{
+	global $context, $settings, $options, $txt, $scripturl;
+
+	echo '
+	<div id="announcement">
+		<form action="', $scripturl, '?action=announce;sa=send" method="post" accept-charset="UTF-8">
+			<div class="cat_bar">
+				<h3 class="catbg">', $txt['announce_title'], '</h3>
+			</div>
+			<div class="information">
+				', $txt['announce_desc'], '
+			</div>
+			<div class="windowbg2">
+				<div class="content">
+					<p>
+						', $txt['announce_this_topic'], ' <a href="', $scripturl, '?topic=', $context['current_topic'], '.0">', $context['topic_subject'], '</a>
+					</p>
+					<ul class="reset">';
+
+	foreach ($context['groups'] as $group)
+		echo '
+						<li>
+							<label for="who_', $group['id'], '"><input type="checkbox" name="who[', $group['id'], ']" id="who_', $group['id'], '" value="', $group['id'], '" checked="checked" class="input_check" /> ', $group['name'], '</label> <em>(', $group['member_count'], ')</em>
+						</li>';
+
+	echo '
+						<li>
+							<label for="checkall"><input type="checkbox" id="checkall" class="input_check" onclick="invertAll(this, this.form);" checked="checked" /> <em>', $txt['check_all'], '</em></label>
+						</li>
+					</ul>
+					<hr class="hrcolor" />
+					<div id="confirm_buttons">
+						<input type="submit" value="', $txt['post'], '" class="button_submit" />
+						<input type="hidden" name="', $context['session_var'], '" value="', $context['session_id'], '" />
+						<input type="hidden" name="topic" value="', $context['current_topic'], '" />
+						<input type="hidden" name="move" value="', $context['move'], '" />
+						<input type="hidden" name="goback" value="', $context['go_back'], '" />
+					</div>
+				</div>
+				<br class="clear_right" />
+			</div>
+		</form>
+	</div>
+	<br />';
+}
+
+function template_announcement_send()
+{
+	global $context, $settings, $options, $txt, $scripturl;
+
+	echo '
+	<div id="announcement">
+		<form action="' . $scripturl . '?action=announce;sa=send" method="post" accept-charset="UTF-8" name="autoSubmit" id="autoSubmit">
+			<div class="windowbg2">
+				<div class="content">
+					<p>', $txt['announce_sending'], ' <a href="', $scripturl, '?topic=', $context['current_topic'], '.0" target="_blank" class="new_win">', $context['topic_subject'], '</a></p>
+					<div class="progress_bar">
+						<div class="full_bar">', $context['percentage_done'], '% ', $txt['announce_done'], '</div>
+						<div class="green_percent" style="width: ', $context['percentage_done'], '%;">&nbsp;</div>
+					</div>
+					<hr class="hrcolor" />
+					<div id="confirm_buttons">
+						<input type="submit" name="b" value="', $txt['announce_continue'], '" class="button_submit" />
+						<input type="hidden" name="', $context['session_var'], '" value="', $context['session_id'], '" />
+						<input type="hidden" name="topic" value="', $context['current_topic'], '" />
+						<input type="hidden" name="move" value="', $context['move'], '" />
+						<input type="hidden" name="goback" value="', $context['go_back'], '" />
+						<input type="hidden" name="start" value="', $context['start'], '" />
+						<input type="hidden" name="membergroups" value="', $context['membergroups'], '" />
+					</div>
+				</div>
+				<br class="clear_right" />
+			</div>
+		</form>
+	</div>
+	<br />
+	<script type="text/javascript"><!-- // --><![CDATA[
+		var countdown = 2;
+		var txt_message = "', $txt['announce_continue'], '";
+		doAutoSubmit();
+	// ]]></script>';
 }
