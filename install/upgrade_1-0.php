9a65a26e2d5dbe410a69107ccd59d21f6158fc45
emanuele <emanuele45@gmail.com>
2015-10-26 22:51:26 +0100

    Parser errors in upgrade files

--- install/upgrade_1-0.php
+++ install/upgrade_1-0.php
@@ -54,6 +54,8 @@ class UpgradeInstructions_upgrade_1_0
 						'ignore'
 					);
 				}
+			),
+			array(
 				'debug_title' => 'More space for email filters...',
 				'function' => function($db, $db_table)
 				{
@@ -68,6 +70,8 @@ class UpgradeInstructions_upgrade_1_0
 						'ignore'
 					);
 				}
+			),
+			array(
 				'debug_title' => 'Possible wrong type for mail_queue...',
 				'function' => function($db, $db_table)
 				{
