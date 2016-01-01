ca7e20b06f31365d1c8c2556666c7e472a6012e1
emanuele <emanuele45@gmail.com>
2016-01-01 22:11:20 +0100

    temp

--- tests/sources/subs/Auth.subs.Test.php
+++ tests/sources/subs/Auth.subs.Test.php
@@ -48,40 +48,4 @@ class TestAuthsubs extends PHPUnit_Framework_TestCase
 		$array = @unserialize($_COOKIE[$cookiename]);
 		$this->assertEquals($array[1], hash('sha256', $this->passwd . $salt));
 	}
-
-	/**
-	 * Validate a user password
-	 */
-	public function test_password()
-	{
-		// Load them with loadExistingMember
-		$user_data = loadExistingMember($this->user, false);
-		$this->assertNotEmpty($user_data);
-
-		// What a form could send in
-		$password = hash('sha256', Util::strtolower($this->user) . un_htmlspecialchars($this->passwd));
-
-		// Check secure and insecure forms
-		$this->assertTrue(validateLoginPassword($password, $user_data['passwd']));
-		$this->assertTrue(validateLoginPassword($this->passwd, $user_data['passwd'], $this->user));
-	}
-
-	/**
-	 * Load a user by email etc.  Then send in a bogus password
-	 */
-	public function test_false_password()
-	{
-		// Get the id with the userByEmail function
-		$user_id = userByEmail($this->useremail);
-
-		// Then load them with loadExistingMember by id
-		$user_data = loadExistingMember($user_id, true);
-		$this->assertEquals($user_data['member_name'], $this->user);
-
-		// Send in a hashed password, like it was stolen
-		$this->assertFalse(validateLoginPassword($user_data['passwd'], $user_data['passwd']));
-
-		// Or just a plain text one
-		$this->assertFalse(validateLoginPassword($this->passwd, $user_data['passwd']));
-	}
 }
